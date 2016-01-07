7d94b5502383d6a004556ce0981a1783cb596d5e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-01-07 20:30:36 +0100

    Add JavaScript for mobile menu

--- themes/papi/source/js/main.js
+++ themes/papi/source/js/main.js
@@ -1,6 +1,6 @@
 // Redirect to the old documentation when selecting a old version.
 if (document.querySelector('.version-select')) {
-  document.querySelector('.version-select').addEventListener('change', function (e) {
+  document.querySelector('.version-select').addEventListener('change', function(e) {
     var version = e.target.value
     if (version.indexOf('3.') !== 0) {
       window.location.assign('https://wp-papi.github.io/docs-' + version + '/')
@@ -8,4 +8,21 @@ if (document.querySelector('.version-select')) {
   });
 }
 
+// Main navigation
+var mobileMenu = document.querySelector('#js-centered-navigation-menu');
+var mobileToggle = document.querySelector('#js-centered-navigation-mobile-menu');
+
+mobileToggle.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  if (mobileMenu.className.indexOf(' show') !== -1) {
+    mobileMenu.className = mobileMenu.className.replace('show', '');
+  } else {
+    mobileMenu.className += ' show';
+  }
+});
+
+mobileToggle.dispatchEvent(new Event('click'));
+
+// Initialize highlight.js
 hljs.initHighlightingOnLoad();
