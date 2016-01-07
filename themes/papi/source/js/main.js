a047841bd9cbb75a736ec02c9c2224720c77c2c1
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-01-07 20:21:19 +0100

    Only bind version select if it exists

--- themes/papi/source/js/main.js
+++ themes/papi/source/js/main.js
@@ -1,9 +1,11 @@
 // Redirect to the old documentation when selecting a old version.
-document.querySelector('.version-select').addEventListener('change', function (e) {
-  var version = e.target.value
-  if (version.indexOf('3.') !== 0) {
-    window.location.assign('https://wp-papi.github.io/docs-' + version + '/')
-  }
-});
+if (document.querySelector('.version-select')) {
+  document.querySelector('.version-select').addEventListener('change', function (e) {
+    var version = e.target.value
+    if (version.indexOf('3.') !== 0) {
+      window.location.assign('https://wp-papi.github.io/docs-' + version + '/')
+    }
+  });
+}
 
 hljs.initHighlightingOnLoad();
